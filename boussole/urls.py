3fdf701923b4cef48549ed5e9546408e65b62612
Achraf UBA <achrafouba2004@gmail.com>
2025-07-11 12:21:10 +0100

    Fixed margin issue

--- boussole/urls.py
+++ boussole/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('', include('compta.urls')),
 ]
 
-# Serve media files during development
+# Serve media and static files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
